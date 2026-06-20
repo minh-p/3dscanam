9953ff601416fba55cc13f2d1bc373596645791c
minh-p <minhisverynormal@gmail.com>
2026-06-20 12:22:23 -0700

    feat(devenv): gitignore, remove extra files
    rm(devenv): remove .cache/clangd

--- amconstruct3d/build/CMakeFiles/unit_tests.dir/src/devenv.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/unit_tests.dir/src/devenv.cpp.o: \
-  /home/hmp/Projects/3dscanam/amconstruct3d/src/devenv.cpp \
-  /home/hmp/Projects/3dscanam/amconstruct3d/include/devenv.hpp
--- amconstruct3d/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hmp/Projects/3dscanam/amconstruct3d
-# Build directory: /home/hmp/Projects/3dscanam/amconstruct3d/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/hmp/Projects/3dscanam/amconstruct3d/build/unit_tests[1]_include.cmake")
