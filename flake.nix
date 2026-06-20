cf58958f777e915f90f2783b57feebf5d2803b66
minh-p <minhisverynormal@gmail.com>
2026-06-20 11:49:32 -0700

    feat(devenv | fix): nixpkgs url

--- flake.nix
+++ flake.nix
@@ -1,24 +1,45 @@
 {
   description = "C++ W/ OpenCV, SDL3, GTest";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-26.06"; };
-  outputs = { self, ... }@inputs:
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+  outputs =
+    { self, ... }@inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
           f {
             inherit system;
             pkgs = import inputs.nixpkgs { inherit system; };
-          });
+          }
+        );
 
-    in {
-      devShells = forEachSupportedSystem ({ pkgs, system }: {
-        default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
-          packages = with pkgs;
-            [ clang-tools cmake gtest self.formatter.${system} opencv sdl3 ]
-            ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ gdb ];
-        };
-      });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }: {
+          default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            packages =
+              with pkgs;
+              [
+                clang-tools
+                cmake
+                gtest
+                self.formatter.${system}
+                opencv
+                sdl3
+              ]
+              ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ gdb ];
+          };
+        }
+      );
       formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
     };
 }
